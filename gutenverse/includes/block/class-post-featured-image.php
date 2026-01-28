136d4ef2263830879ac9aaa58396112da22e59d9
Ikhlashmulya <nurahmanmulya@gmail.com>
2026-01-28 15:30:15 +0800

    [fix/fix-bug] add isset check for lazyLoad to prevent PHP Warning

--- gutenverse/includes/block/class-post-featured-image.php
+++ gutenverse/includes/block/class-post-featured-image.php
@@ -39,7 +39,8 @@ class Post_Featured_Image extends Block_Abstract {
 		$post_featured   = get_the_post_thumbnail_url( $post_id, $image_size['value'] );
 		$custom_classes  = $this->get_custom_classes();
 		$content         = '';
-		$image_load      = Options::get_instance()->get_image_load( 'lazy', $this->attributes['lazyLoad'], $this->attributes['imageLoad'] );
+		$lazy_load       = isset( $this->attributes['lazyLoad'] ) ? $this->attributes['lazyLoad'] : false;
+		$image_load      = Options::get_instance()->get_image_load( 'lazy', $lazy_load, $this->attributes['imageLoad'] );
 
 		if ( ! empty( $post_featured ) ) {
 			$content = get_the_post_thumbnail( $post_id, $image_size['value'], array( 'loading' => $image_load ) );
