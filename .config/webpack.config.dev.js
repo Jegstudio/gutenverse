7b69678120400fc75067fbf61df39f84d048a25d
Arya1Putra <putra1arya@gmail.com>
2026-02-05 14:14:20 +0800

    [dev/remove-unibiz-banner] remove form dari gutenverse

--- .config/webpack.config.dev.js
+++ .config/webpack.config.dev.js
@@ -2,12 +2,10 @@ const { blocks } = require('./scripts/blocks');
 const { dashboard } = require('./scripts/dashboard');
 const { wizard } = require('./scripts/wizard');
 const { frontendModular } = require('./scripts/frontend-modular');
-const { formFallback } = require('./scripts/frontend-form-fallback');
 
 module.exports = [
     blocks,
     dashboard,
     wizard,
     frontendModular,
-    formFallback
 ];
