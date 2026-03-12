af4672378dbea1d440641466e85c839dfa8caa42
Yoga <yoga@jegstudio.com>
2026-03-12 14:58:09 +0800

    [dev/migrate-to-dynamic] fix team block save.js

--- src/editor/blocks/team/save.js
+++ src/editor/blocks/team/save.js
@@ -1,35 +1,6 @@
-import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
-import { classnames } from 'gutenverse-core/components';
-import { withAnimationAdvanceScript, withMouseMoveEffectScript } from 'gutenverse-core/hoc';
-import { useAnimationFrontend } from 'gutenverse-core/hooks';
-import { useDisplayFrontend } from 'gutenverse-core/hooks';
-import { useAnimationAdvanceData } from 'gutenverse-core/hooks';
-import { compose } from '@wordpress/compose';
 
-const save = compose(
-    withAnimationAdvanceScript('team'),
-    withMouseMoveEffectScript
-)((props) => {
-    const { attributes } = props;
-    const { elementId } = attributes;
+import { InnerBlocks } from '@wordpress/block-editor';
 
-    const advanceAnimationData = useAnimationAdvanceData(attributes);
-    const animationClass = useAnimationFrontend(attributes);
-    const displayClass = useDisplayFrontend(attributes);
-
-    const className = classnames(
-        'guten-element',
-        'guten-team',
-        elementId,
-        animationClass,
-        displayClass,
-    );
-
-    return (
-        <div {...useBlockProps.save({ className, ...advanceAnimationData })}>
-            <InnerBlocks.Content />
-        </div>
-    );
-});
-
-export default save;
+export default function save() {
+    return <InnerBlocks.Content />;
+}
