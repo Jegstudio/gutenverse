771bc962bc7208941a6f7501ae1e13922b5e059d
Yoga <yoga@jegstudio.com>
2026-03-12 17:59:49 +0800

    [dev/migrate-to-dynamic] fix social icons after migrate

--- src/editor/blocks/social-icon/index.js
+++ src/editor/blocks/social-icon/index.js
@@ -5,13 +5,42 @@ import metadata from './block.json';
 import { IconSocialSVG } from '../../../assets/icon/index';
 import saveV1 from './deprecated/v1/save';
 import saveV2 from './deprecated/v2/save';
+import saveV3 from './deprecated/v3/save';
 
 const { name, attributes } = metadata;
 
 export { metadata, name };
 
-const v2Attributes = {
+// OLD Attributes with sources for deprecations
+const v3Attributes = {
     ...attributes,
+    url: {
+        type: 'string',
+        source: 'attribute',
+        selector: 'a',
+        attribute: 'href'
+    },
+    linkTarget: {
+        type: 'string',
+        source: 'attribute',
+        selector: 'a',
+        attribute: 'target'
+    },
+    rel: {
+        type: 'string',
+        source: 'attribute',
+        selector: 'a',
+        attribute: 'rel'
+    },
+    text: {
+        type: 'string',
+        source: 'html',
+        selector: 'span'
+    },
+};
+
+const v2Attributes = {
+    ...v3Attributes,
     icon: {
         type: 'string',
         source: 'attribute',
@@ -27,12 +56,25 @@ export const settings = {
     save,
     deprecated: [
         {
-            attributes: v2Attributes,
-            save: saveV2
+            attributes: v3Attributes,
+            save: saveV3,
+            migrate(attr) {
+                return attr;
+            },
         },
         {
             attributes: v2Attributes,
-            save: saveV1
-        }
-    ]
+            save: saveV2,
+            migrate(attr) {
+                return attr;
+            },
+        },
+        {
+            attributes: v2Attributes,
+            save: saveV1,
+            migrate(attr) {
+                return attr;
+            },
+        },
+    ],
 };
--- src/editor/blocks/social-icons/save.js
+++ src/editor/blocks/social-icons/save.js
@@ -1,49 +1,6 @@
 
-import { classnames } from 'gutenverse-core/components';
-import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
-import { compose } from '@wordpress/compose';
-import { withAnimationAdvanceScript, withMouseMoveEffectScript } from 'gutenverse-core/hoc';
-import { useAnimationFrontend } from 'gutenverse-core/hooks';
-import { useDisplayFrontend } from 'gutenverse-core/hooks';
-import { useAnimationAdvanceData } from 'gutenverse-core/hooks';
+import { InnerBlocks } from '@wordpress/block-editor';
 
-const save = compose(
-    withAnimationAdvanceScript('social-icons'),
-    withMouseMoveEffectScript
-)((props) => {
-    const {
-        attributes
-    } = props;
-
-    const {
-        elementId,
-        showText,
-        shape,
-        color,
-        orientation,
-    } = attributes;
-
-    const advanceAnimationData = useAnimationAdvanceData(attributes);
-    const animationClass = useAnimationFrontend(attributes);
-    const displayClass = useDisplayFrontend(attributes);
-
-    const className = classnames(
-        'guten-element',
-        'guten-social-icons',
-        elementId,
-        shape,
-        orientation,
-        color,
-        animationClass,
-        displayClass,
-        {
-            'show-text': showText,
-        },
-    );
-
-    return <div {...useBlockProps.save({ className, ...advanceAnimationData })}>
-        <InnerBlocks.Content/>
-    </div>;
-});
-
-export default save;
+export default function save() {
+    return <InnerBlocks.Content />;
+}
