17393434cc2f998eafdc5cb6963554e974875b4f
Arya1Putra <putra1arya@gmail.com>
2026-03-16 15:55:05 +0800

    [dev/migrate-to-dynamic] migrate icon list item

---
+++ gutenverse/includes/block/class-icon-list-item.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Icon List Item Block class
+ *
+ * @author Jegstudio
+ * @since 1.0.0
+ * @package gutenverse\block
+ */
+
+namespace Gutenverse\Block;
+
+use Gutenverse\Framework\Block\Block_Abstract;
+
+/**
+ * Class Icon List Item Block
+ *
+ * @package gutenverse\block
+ */
+class Icon_List_Item extends Block_Abstract {
+
+	/**
+	 * Render content
+	 *
+	 * @return string
+	 */
+	public function render_content() {
+		$element_id  = $this->get_element_id();
+		$url         = isset( $this->attributes['url'] ) ? $this->attributes['url'] : '';
+		$link_target = isset( $this->attributes['linkTarget'] ) ? $this->attributes['linkTarget'] : '';
+		$rel         = isset( $this->attributes['rel'] ) ? $this->attributes['rel'] : '';
+		$aria_label  = isset( $this->attributes['ariaLabel'] ) ? $this->attributes['ariaLabel'] : '';
+		$icon        = isset( $this->attributes['icon'] ) ? $this->attributes['icon'] : '';
+		$icon_type   = isset( $this->attributes['iconType'] ) ? $this->attributes['iconType'] : 'icon';
+		$icon_svg    = isset( $this->attributes['iconSVG'] ) ? $this->attributes['iconSVG'] : '';
+		$hide_icon   = isset( $this->attributes['hideIcon'] ) ? $this->attributes['hideIcon'] : false;
+		$text        = isset( $this->attributes['content'] ) ? $this->attributes['content'] : '';
+
+		if ( empty( $text ) ) {
+			$text = isset( $this->attributes['text'] ) ? $this->attributes['text'] : '';
+		}
+
+		$href = apply_filters(
+			'gutenverse_dynamic_generate_dynamic_pattern_link',
+			$url,
+			'dynamicUrl',
+			$this->attributes,
+			$element_id
+		);
+
+		$icon_html = '';
+		if ( ! $hide_icon ) {
+			if ( 'svg' === $icon_type && $icon_svg ) {
+				// phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_decode, WordPress.Security.EscapeOutput.OutputNotEscaped
+				$icon_html = '<div class="gutenverse-icon-svg">' . base64_decode( $icon_svg ) . '</div>';
+			} elseif ( $icon ) {
+				$icon_html = '<i class="' . esc_attr( $icon ) . '" aria-hidden="true"></i>';
+			}
+		}
+
+		$text_class = 'list-text' . ( $hide_icon ? ' no-icon' : '' );
+
+		$content  = '<div class="list-divider"></div>';
+		$content .= '<a id="' . esc_attr( $element_id ) . '" href="' . esc_url( $href ) . '" target="' . esc_attr( $link_target ) . '" rel="' . esc_attr( $rel ) . '" aria-label="' . esc_attr( $aria_label ) . '">';
+		$content .= $icon_html; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		$content .= '<span class="' . esc_attr( $text_class ) . '">' . $text . '</span>';
+		$content .= '</a>';
+
+		return $content;
+	}
+
+	/**
+	 * Render view in editor
+	 */
+	public function render_gutenberg() {
+		return $this->render_content();
+	}
+
+	/**
+	 * Render view in frontend
+	 */
+	public function render_frontend() {
+		$element_id      = $this->get_element_id();
+		$display_classes = $this->set_display_classes();
+		$animation_class = $this->set_animation_classes();
+		$custom_classes  = $this->get_custom_classes();
+
+		$class_name = trim( 'guten-element guten-icon-list-item ' . $element_id . ' ' . $animation_class . ' ' . $display_classes . ' ' . $custom_classes );
+
+		return '<div class="' . esc_attr( $class_name ) . '">' . $this->render_content() . '</div>';
+	}
+}
