9c173a64f000d6a3aefaec9cd65219dbc545d5e3
Arya1Putra <putra1arya@gmail.com>
2026-03-24 12:07:58 +0800

    [dev/migrate-to-dynamic] fix icon save

--- src/editor/blocks/icon/index.js
+++ src/editor/blocks/icon/index.js
@@ -1,11 +1,11 @@
 
 import edit from './edit';
-import saveV2 from './save';
+import save from './save';
 import metadata from './block.json';
 import { IconSVG } from '../../../assets/icon/index';
 import example from './data/example';
 import saveV1 from './deprecated/v1/save';
-import save from './deprecated/v2/save';
+import saveV2 from './deprecated/v2/save';
 
 const { name, attributes, supports } = metadata;
 
