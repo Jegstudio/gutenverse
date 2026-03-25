12c711be1867f48cd6b6ebf7f40b00124982ecce
Yoga <yoga@jegstudio.com>
2026-03-25 16:20:25 +0800

    [dev/migrate-to-dynamic] migrate popup block

--- src/editor/blocks/popup-builder/index.js
+++ src/editor/blocks/popup-builder/index.js
@@ -7,6 +7,7 @@ import { IconPopupBuilderSVG } from '../../../assets/icon/index';
 import saveV1 from './deprecated/v1/save';
 import saveV2 from './deprecated/v2/save';
 import saveV3 from './deprecated/v3/save';
+import saveV4 from './deprecated/v4/save';
 
 const { name, attributes, supports } = metadata;
 
@@ -83,7 +84,12 @@ export const settings = {
         {
             attributes,
             supports,
-            save: saveV3
-        }
+            save: saveV3,
+        },
+        {
+            attributes,
+            supports,
+            save: saveV4,
+        },
     ]
 };
