5172591e1f04d32b36e1d6dff504a68c0dfd56ae
Yoga <yoga@jegstudio.com>
2026-03-25 13:39:46 +0800

    [dev/migrate-to-dynamic] migrate text paragraph block

---
+++ src/editor/blocks/text-paragraph/deprecated/v1/save.js
@@ -0,0 +1,46 @@
+import { compose } from '@wordpress/compose';
+
+import { classnames } from 'gutenverse-core/components';
+import { withAnimationAdvanceScript, withMouseMoveEffectScript } from 'gutenverse-core/hoc';
+import { useAnimationFrontend } from 'gutenverse-core/hooks';
+import { useDisplayFrontend } from 'gutenverse-core/hooks';
+import { useAnimationAdvanceData } from 'gutenverse-core/hooks';
+import { RichText } from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
+
+const save = compose(
+    withAnimationAdvanceScript('text-paragraph'),
+    withMouseMoveEffectScript
+)((props) => {
+    const {
+        attributes
+    } = props;
+
+    const {
+        elementId,
+        paragraph
+    } = attributes;
+
+    const advanceAnimationData = useAnimationAdvanceData(attributes);
+    const animationClass = useAnimationFrontend(attributes);
+    const displayClass = useDisplayFrontend(attributes);
+
+    const className = classnames(
+        'guten-element',
+        'gutenverse-text',
+        elementId,
+        animationClass,
+        displayClass,
+    );
+    return (
+        <RichText.Content
+            tagName={'p'}
+            className={className}
+            aria-label={__('Text Paragraph', 'gutenverse')}
+            value={paragraph}
+            {...advanceAnimationData}
+        />
+    );
+});
+
+export default save;
