ce1cccc1864d42482d0895e9ae0deeeb8eaedc07
Gowinda Kadek <gowinda@jegstudio.com>
2026-04-02 10:39:58 +0800

    [trunk] fix the selector to only apply in post block type 5

--- src/editor/blocks/post-block/panels/panel-category.js
+++ src/editor/blocks/post-block/panels/panel-category.js
@@ -63,7 +63,8 @@ export const categoryPanel = (props) => {
                 {
                     'type': 'color',
                     'id': 'categoryBackground',
-                    'selector': `.${elementId} .guten-postblock .guten-post-category > span a`,
+                    'selector': `.${elementId}:not(.postblock-type-5) .guten-postblock .guten-post-category,
+                        .${elementId}.postblock-type-5 .guten-post-category > span a`,
                     'properties': [
                         {
                             'name' : 'background-color',
@@ -124,7 +125,8 @@ export const categoryPanel = (props) => {
                 {
                     'type': 'border',
                     'id': 'categoryBorder',
-                    'selector': `.${elementId} .guten-postblock .guten-post-category > span a`,
+                    'selector': `.${elementId}:not(.postblock-type-5) .guten-postblock .guten-post-category,
+                        .${elementId}.postblock-type-5 .guten-post-category > span a`,
                 }
             ]
         },
@@ -138,7 +140,8 @@ export const categoryPanel = (props) => {
                 {
                     'type': 'borderResponsive',
                     'id': 'categoryBorderResponsive',
-                    'selector': `.${elementId} .guten-postblock .guten-post-category > span a`,
+                    'selector': `.${elementId}:not(.postblock-type-5) .guten-postblock .guten-post-category,
+                        .${elementId}.postblock-type-5 .guten-post-category > span a`,
                 }
             ]
         },
@@ -156,7 +159,8 @@ export const categoryPanel = (props) => {
                             'valueType': 'direct'
                         }
                     ],
-                    'selector': `.${elementId} .guten-postblock .guten-post-category > span a`,
+                    'selector': `.${elementId}:not(.postblock-type-5) .guten-postblock .guten-post-category,
+                        .${elementId}.postblock-type-5 .guten-post-category > span a`,
                 }
             ]
         },
